2f0cb53194230e562007ad8af0d8667fa0061497
Adam Glasgall <adam@crossproduct.net>
2013-09-11 17:19:21 -0400

    rewrite the main parsing loop to use named captures for clarity

--- config.rb
+++ config.rb
@@ -43,35 +43,41 @@ def load_config(path, overrides=[])
     f.each do |line|
       # eat blank and comment lines
       next if (line =~ /^\s*$/ || line =~ /\s*;.*$/)
-      if state == :toplevel
-        if line =~ /^\s*\[(\w+)\]/
-          current_section_name = $1.to_sym
-          sections[current_section_name] = ConfigHash.new
-          state = :section
-        else
-          raise ConfigSyntaxError, "Syntax error at toplevel"
+
+      if /^\s* # eat leading whitespace
+                     \[(?<section_name>\w+)\] # capture the section name
+         /x =~ line
+        current_section_name = section_name.to_sym
+        sections[current_section_name] = ConfigHash.new
+      elsif /^\s* # eat leading whitespace
+              (?<name>\w+)(<(?<override>\w+)>)? # capture parameter
+                                                # name and optional
+                                                # override
+              \s*=\s* # eat whitespace on either side of the =
+              (?<raw_value>.+) # capture parameter value, making no
+                               # assumptions about what characters are
+                               # allowed in a value, except:
+              (?:\s*;.*)? # if we see a semicolon (with none or any
+                          # whitespace immediately before it), treat
+                          # the rest of the line as a comment
+            /x =~ line
+        value = parse_raw_value(raw_value)
+        # String is also enumerable and responds to most of the same methods,
+        # but we still want to handle arrays separately, so we have to break duck typing
+        if value.is_a? Array
+          value = value.map { |v| parse_raw_value(v) }
         end
-      elsif state == :section
-        if line =~ /^\s*(\w+)(<(\w+)>)?\s*=\s*(.+)(?:\s*;.*)?/
-          name = $1
-          override = $3
-          raw_value = $4
-          value = parse_raw_value(raw_value)
-          # String is also enumerable and responds to most of the same methods,
-          # but we still want to handle arrays separately, so we have to break duck typing
-          if value.is_a? Array
-            value = value.map { |v| parse_raw_value(v) }
-          end
-          value = canonicalize_boolean(value)
-          if (override && !override.empty? && override_set.member?(override)) || (!override || override.empty?)
-            sections[current_section_name][name.to_sym] = value
-          end
-        elsif line =~ /^\s*\[(\w+)\]/
-          current_section_name = $1.to_sym
-          sections[current_section_name] = ConfigHash.new
-        else
-          raise ConfigSyntaxError, "Syntax error in section '#{current_section_name.to_s}'"
+        value = canonicalize_boolean(value)
+        # if there's an override, unless it's in the set of overrides
+        # passed in, ignore it. otherwise, set the value.  we don't
+        # bother storing any other other overrides for the value,
+        # since they're not needed after the file has been read.
+        if (override && !override.empty? && override_set.member?(override)) || 
+            (!override || override.empty?)
+          sections[current_section_name][name.to_sym] = value
         end
+      else
+        raise ConfigSyntaxError, "Syntax error on line #{f.lineno}"
       end
     end
   end
